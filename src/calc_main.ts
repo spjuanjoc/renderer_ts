128c0843021844d385231189bc70716189138e37
Juan Jose Castellanos <juan.castellanos@smartmatic.com>
2020-04-20 15:31:15 -0500

    set onclick action for all numbers

--- src/calc_main.ts
+++ src/calc_main.ts
@@ -1,1 +1,14 @@
 console.log('Neumorphic calculator');
+
+let calculator = document.querySelector("input");
+
+let buttons = document.querySelectorAll(".num__key");
+
+if(buttons){
+    buttons.forEach( button => {
+        button.addEventListener('click',() => {
+            calculator.value = calculator.value !== "0" ?
+                calculator.value + button.textContent : button.textContent;
+        });
+    });
+}
