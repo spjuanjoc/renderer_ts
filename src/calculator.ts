7773438caab3515faa3ac231eb4a6c54b8af16fb
Juan Jose Castellanos <juan.castellanos@smartmatic.com>
2020-04-20 17:41:26 -0500

    create the rest of the functions but empty
    Fill the percentage func

--- src/calculator.ts
+++ src/calculator.ts
@@ -7,15 +7,7 @@ const equal = document.querySelector(".eq__key");
 const clear = document.querySelector(".op__key[op=clear]");
 
 let result = "result";
-
-if(numbers){
-    numbers.forEach(numberButton => {
-        numberButton.addEventListener('click',() => {
-            display.value = display.value !== "0" ?
-                display.value + numberButton.textContent : numberButton.textContent;
-        });
-    });
-}
+const buffer:string[] = [];
 
 const operations =['add','subtract','multiply','divide','percent'];
 let opMap = new Map<string,string>();
@@ -28,63 +20,68 @@ opMap.set('negate','--');
 
 let callbacksMap = new Map<string,()=>void >();
 callbacksMap.set('add',()=>{ add(); });
+callbacksMap.set('subtract', ()=> {subtract();});
+callbacksMap.set('multiply', ()=> {multiply();});
+callbacksMap.set('divide', ()=> {divide();});
+callbacksMap.set('percent',()=> {percent();});
+callbacksMap.set('negate', ()=> {negate();});
 
-function add(){
-    console.log('add!!');
+if(numbers){
+    numbers.forEach(numberButton => {
+        numberButton.addEventListener('click',() => {
+            display.value = display.value !== "0" ?
+                display.value + numberButton.textContent : numberButton.textContent;
+            let currentValue = parseFloat(display.value);
+            buffer.push(display.value);
+        });
+    });
 }
 
 for (let operation of operations){
     console.log('operations');
     let operator = document.querySelector(`.op__key[op=${operation}]`);
     operator.addEventListener('click', () => {
-        operate(operation);
-        console.log('clicked: ', operation, ' for: ', display.value);
+        buffer.push()
+        console.log('clicked:', operation, ' for: ', display.value);
         display.value += opMap.get(operation);
         callbacksMap.get(operation)();
-        // operator();
     });
 }
-const buffer:string[] = [];
-
-function operate(operation) {
-    let currentValue = parseFloat(display.value);
-    console.log('current value: ', currentValue );
-
-    if(operation == "percent"){
-        currentValue *= 0.01;
-        display.value = currentValue.toString();
-        console.log('percent operation called: ', currentValue, display.value);
-    }
-    else{
-        if(buffer.length == 0){
-            let lhs = currentValue;
-        }
-        // if (buffer && buffer.length !== 0){
-        //     buffer.push({value: currentValue});
-        //     const result = evaluate(buffer);
-        //     display.value = ""
-        // }
-    }
-}
-
-function evaluate(op:string):string{
-
-    if (op == 'add'){
-        // add()
-    }
-    return "";
-}
-
-// function add(lhs:number, rhs:number):number{
-//     return lhs + rhs;
-// }
-
 
 equal.addEventListener('click', () => {
     display.value = result;
 });
 
-
 clear.addEventListener('click', () => {
     display.value = "0";
 })
+
+//Math functions
+function add(){
+    console.log('add!!');
+    result = 'add!!';
+}
+function subtract(){
+    console.log('subtract!!');
+    result = 'subtract!!';
+}
+function multiply(){
+    console.log('multiply!!');
+    result = 'multiply!!';
+}
+function divide(){
+    console.log('divide!!');
+    result = 'divide!!';
+}
+function percent() {
+    console.log('percent!!')
+    result = 'percent!!'
+    let currentValue = parseFloat(display.value);
+    currentValue *= 0.01;
+    display.value = currentValue.toString();
+    console.log('percent operation called: ', currentValue, display.value);
+}
+function negate(){
+    console.log('negate!!');
+    result = 'negate!!';
+}
