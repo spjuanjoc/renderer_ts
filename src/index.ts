dba78affb2ba0c849d790d3a8168e2100258c34d
Juan Jose Castellanos <juan.castellanos@smartmatic.com>
2020-04-20 10:51:00 -0500

    add basic renderer for a couple of windows
    Configure the tsconfig to accept dividing the project in modules

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,31 @@ import {app, BrowserWindow} from 'electron'
 
 console.log('Hello electron from typescript');
 
-let window;
+let window1;
+let window2;
 
 app.on('ready', () => {
-    window = new BrowserWindow({
+    window1 = new BrowserWindow({
         width: 600,
         height: 480,
         darkTheme: true,
-        backgroundColor: '#A9F5F2'
+        backgroundColor: '#A9F5F2'/*,
+        webPreferences:{
+            nodeIntegration:true
+        }*/
     });
-    window.loadFile('../index.html');
+    window1.loadFile('../renderers/main.html');
+
+    window2 = new BrowserWindow({
+        width: 600,
+        height: 480,
+        darkTheme: true,
+        backgroundColor: '#FFF'
+    });
+    window2.loadFile('../renderers/second.html');
 });
 
 app.on('window-all-closed', () => {
     app.quit()
 })
+
---
+++ src/main.ts
@@ -0,0 +1,22 @@
+console.log('main window');
+
+const BrowserWindow = require('electron').remote.BrowserWindow;
+// import {remote, BrowserWindow} from 'electron';
+//
+const newWinButton = document.getElementById('newWindow');
+
+if (newWinButton) {
+    newWinButton.addEventListener('click', () => {
+        let window3 = new BrowserWindow({
+            width: 600,
+            height: 480,
+            darkTheme: true,
+            backgroundColor: '#AAA'/*,
+            webPreferences:{
+                nodeIntegration:true
+            }*/
+        });
+
+        window3.loadFile('../renderers/second.html');
+    });
+}
